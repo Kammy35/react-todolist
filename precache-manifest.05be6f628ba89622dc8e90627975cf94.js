2996f17bd1505de2ead0ea85ffdd3364cc302c0f
kamran nawaz <kammy007@hotmail.com>
2019-11-21 14:39:52 +0000

    Updates

---
+++ precache-manifest.05be6f628ba89622dc8e90627975cf94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4515264c489b69d9671edbbe9b5350",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "525e1df659731da5bdbd",
+    "url": "/react-todolist/static/css/main.4707e6c7.chunk.css"
+  },
+  {
+    "revision": "50a7414d60fd57bfe34a",
+    "url": "/react-todolist/static/js/2.b0baffcc.chunk.js"
+  },
+  {
+    "revision": "525e1df659731da5bdbd",
+    "url": "/react-todolist/static/js/main.f8adc326.chunk.js"
+  },
+  {
+    "revision": "48e2e8ef4589774683f3",
+    "url": "/react-todolist/static/js/runtime-main.9739803e.js"
+  }
+]);
